2d582471e60b48710e34eaa6d42b2180b6df6ca0
BTSBRINGMEHERE <mynameisyyb@naver.com>
2022-12-26 23:26:03 +0900

    store을 배열에 삽입시킴
    이게 초반에 바로 들어가는게 아니라서
    처음에 undefined 값이 들어가면 전체 코드가 에러가 생김... store을 배열로 이쁘게 만들고 싶지만... 어떻게 방법을 모르겠음 ㅠㅠ

--- src/components/main/MainSection.tsx
+++ src/components/main/MainSection.tsx
@@ -1,45 +1,53 @@
 import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../store/hooks"
 import { fetchStore } from "../../store/storeSlice"
+import { Link } from "react-router-dom"
+import { StoreListType } from "../../api/storeAPI"
 
 const MainSection = () => {
   const { fakeStore } = useAppSelector((state) => state.store)
+
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     dispatch(fetchStore())
   }, [dispatch])
 
-  console.log(fakeStore)
+  const store = new Array(0)
+
+  for (let i = 0; i < 20; i++) {
+    store[i] = fakeStore[i]
+  }
 
   return (
     <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto h-max">
       <h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">
         패션
       </h2>
-      <div
-        className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list"
-        data-scroll="true"
-      >
-        <a
-          className="card card-bordered border-gray-200 dark:border-gray-800 card-compact lg:card-normal"
-          href="/product/1"
+
+      {store.map((item: StoreListType) => (
+        <div
+          className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list"
+          data-scroll="true"
         >
-          <figure className="flex h-80 bg-white overflow-hidden">
-            <img
-              src="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
-              alt="상품 이미지"
-              className="transition-transform duration-300"
-            />
-          </figure>
-          <div className="card-body bg-gray-100 dark:bg-gray-700">
-            <p className="card-title text-base">
-              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-            </p>
-            <p className="text-base">$110</p>
-          </div>
-        </a>
-      </div>
+          <Link
+            className="card card-bordered border-gray-200 dark:border-gray-800 card-compact lg:card-normal"
+            to={`product/:${item.id}`}
+          >
+            <figure className="flex h-80 bg-white overflow-hidden">
+              <img
+                src={item.image}
+                alt="상품 이미지"
+                className="transition-transform duration-300"
+              />
+            </figure>
+            <div className="card-body bg-gray-100 dark:bg-gray-700">
+              <p className="card-title text-base">{item.title}</p>
+              <p className="text-base">{item.price}</p>
+            </div>
+          </Link>
+        </div>
+      ))}
     </section>
   )
 }
--- src/pages/HeaderPage.tsx
+++ src/pages/HeaderPage.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react"
 import HeaderEtc from "../components/header/HeaderEtc"
 import HeaderMain from "../components/header/HeaderMain"
 import HeaderSelect from "../components/header/HeaderSelect"
+import { useAppDispatch } from "../store/hooks"
+import { fetchStore } from "../store/storeSlice"
 
 const HeaderPage = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchStore())
+  }, [dispatch])
   return (
     <>
       <div className="fixed z-10 w-full navbar shadow-lg bg-white dark:bg-neutral text-neutral-content ">
